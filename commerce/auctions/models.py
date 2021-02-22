073060f6002b8908d97ca93f188ddc96ac30f7db
Emilien Dre <emilien.dreyer@gmail.com>
2021-02-22 10:49:55 +0100

    Classes created

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,27 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+class Auction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="seller")
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, related_name="category")
+    title = models.CharField(max_length=64)
+    description = models.TextField(max_length=1000)
+    date = models.DateTimeField(auto_now_add=True)
+    start_price = models.IntegerField()
+    image_url = models.URLField()
+
+class Bid(models.Model):
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="bided")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bider")
+    date = models.DateTimeField(auto_now_add=True)
+    price = models.IntegerField()
+
+class Comment(models.Model):
+    auction = models.ForeignKey(Auction, on_delete=models.DO_NOTHING, related_name="commented")
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name="commenter")
+    date = models.DateTimeField(auto_now_add=True)
+    text = models.CharField(max_length=500)
